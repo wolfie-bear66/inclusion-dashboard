167314322cc10649b6168631658950774df66e15
wolfie-bear66 <yatesstuart66@gmail.com>
2026-06-25 18:52:43 +0100

    fix: bundle Tabler icons locally to avoid Edge tracking prevention block

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Analytics } from '@vercel/analytics/react'
+import '@tabler/icons-webfont/dist/tabler-icons.min.css'
 import './index.css'
 import App from './App.jsx'
 
